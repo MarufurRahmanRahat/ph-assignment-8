fcb8b67832a569ada1758bb679875b5a88082433
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-10-08 09:59:48 +0600

    necessary pages and routes for that pages added also some common daisy file name added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <n</nav>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,33 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import Home from "../Pages/Home";
+import Apps from "../Pages/Apps";
+import AppDetails from "../Pages/AppDetails";
+import Error from "../Pages/Error";
+import Installation from "../Pages/Installation";
 
 
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Home />,
+  },
+  {
+    path: '/apps',
+    element: <Apps />,
+  },
+  {
+    path: '/app-details',
+    element: <AppDetails />,
+  },
+  {
+    path: '/error',
+    element: <Error />,
+  },
+  {
+    path: '/installation',
+    element: <Installation />,
   },
 ])
 
